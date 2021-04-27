12f0eb12c4e0f88bd0b41089b91a0afbeadc5988
NSPatton <greengun15@gmail.com>
2021-04-27 15:42:27 -0500

    created character module

---
+++ src/modules/CharacterModule.js
@@ -0,0 +1,37 @@
+const remoteURL = "http://localhost:8088"
+
+export const getCharacterById = (charId) => {
+    return fetch(`${remoteURL}/characters/${charId}`)
+    .then(response => response.json())
+}
+
+export const getAllCharacters = () => {
+    return fetch(`${remoteURL}/characters`)
+    .then(response => response.json())
+}
+
+export const addCharacter = (newCharacter) => {
+    return fetch(`${remoteURL}/characters`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newCharacter)
+    }).then(response => response.json())
+}
+
+export const deleteCharacter = (id) => {
+    return fetch(`${remoteURL}/characters/${id}`, {
+        method: "DELETE"
+    }).then(result => result.json())
+}
+
+export const updateCharacter = (editedCharacter) => {
+    return fetch(`${remoteURL}/characters/${editedCharacter.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(editedCharacter)
+    }).then(data => data.json())
+}
